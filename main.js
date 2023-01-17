cbb6a57fd85b08e9d91a8c70af02f95cf60de458
High <97538467+Onlyfansuser@users.noreply.github.com>
2023-01-17 13:08:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
     
     function init() {
-        totalSeconds = Math.floor((new Date('12/14/2022') - new Date()) / 1000); 
+        totalSeconds = Math.floor((new Date('01/25/2023') - new Date()) / 1000); 
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
